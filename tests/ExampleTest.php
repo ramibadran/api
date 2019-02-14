891569af3309f784ac9861a84b7cdb25872f8c99
Rami Badran <ramibadran.82@gmail.com>
2019-02-14 14:15:38 +0200

    Add validation and README file

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -12,24 +12,23 @@ class ExampleTest extends TestCase{
              ->see('Laravel 5');
     }
     
-    public function testSuccessCase(){
-        $this->json('get', '/api/v1/hotels')
-        ->assertStatus(200)
-        ->assertJson([
-            'city'           => 'AMM',
-            'fromDate'       => '2019-02-13',
-            'toDate'         => '2019-02-14',
-            'numberOfAdults' => 2
-        ]);
+    public function testGetAllHotelsSuccess(){
+        $response = $this->json('get', '/api/v1/hotels', ['city'=>'AMM','fromDate' => '2019-02-13', 'toDate' => 'test123','numberOfAdults'=>2]);
+        $response->assertStatus(200);
     }
     
-    public function testMissingInputCase(){
-        $this->json('get', '/api/v1/hotels')
-        ->assertStatus(412)
-        ->assertJson([
-            'fromDate'       => '2019-02-13',
-            'toDate'         => '2019-02-14',
-            'numberOfAdults' => 2
-        ]);
+    public function testGetAllHotelsPredefinedConditions(){
+        $response = $this->json('get', '/api/v1/hotels', ['fromDate' => '2019-02-13', 'toDate' => 'test123','numberOfAdults'=>2]);
+        $response->assertStatus(200);
+    }
+    
+    public function testTokenGenerationSuccessHotels(){
+        $response = $this->json('POST', '/api/v1/token', ['username' => 'ramibadran.82@gmail.com', 'password' => 'test123']);      
+        $response->assertStatus(200);
+    }
+    
+    public function testTokenGenerationFailHotels(){
+        $response = $this->json('POST', '/api/v1/token', ['username' => 'ramibadran.81@gmail.com', 'password' => 'test123']);
+        $response->assertStatus(401);
     }
 }
