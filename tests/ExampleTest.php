f3561e6730c1216257c981e5c0fccdae57c3470c
Rami Badran <ramibadran.82@gmail.com>
2019-02-14 12:11:25 +0200

    Add validation and README file

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,20 +1,35 @@
 <?php
 
 
-
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends Illuminate\Foundation\Testing\TestCase{
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
-    {
+class ExampleTest extends TestCase{
+   
+    public function testBasicExample(){
         $this->visit('/')
              ->see('Laravel 5');
+    }
+    
+    public function testSuccessCase(){
+        $this->json('get', '/api/v1/hotels')
+        ->assertStatus(200)
+        ->assertJson([
+            'city'           => 'AMM',
+            'fromDate'       => '2019-02-13',
+            'toDate'         => '2019-02-14',
+            'numberOfAdults' => 2
+        ]);
+    }
+    
+    public function testMissingInputCase(){
+        $this->json('get', '/api/v1/hotels')
+        ->assertStatus(412)
+        ->assertJson([
+            'fromDate'       => '2019-02-13',
+            'toDate'         => '2019-02-14',
+            'numberOfAdults' => 2
+        ]);
     }
 }
